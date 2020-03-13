f7c833ffbe1e679379d6d2fd2364f09fbc0188a9
QYB <40496034+QiuYueBaiJXW@users.noreply.github.com>
2020-03-13 15:59:33 +0800

    Update xuexitongDownloader.py

--- xuexitongDownloader.py
+++ xuexitongDownloader.py
@@ -1,8 +1,8 @@
 import requests
 import sys
 
-headers = {
-  
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987\
+	    .116 Safari/537.36',
 }
 objid = sys.argv[1]
 type = sys.argv[2] 
